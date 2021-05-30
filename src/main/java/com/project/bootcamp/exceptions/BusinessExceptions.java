a49fa99c57b00f009dc89763d7282fd8c6d6eaf6
erika <hELO300816>
2021-05-30 18:52:28 -0300

    criação do repositório com o código do bootcamp Santander

---
+++ src/main/java/com/project/bootcamp/exceptions/BusinessExceptions.java
@@ -0,0 +1,13 @@
+package com.project.bootcamp.exceptions;
+
+
+import javax.swing.plaf.PanelUI;
+
+public class BusinessExceptions extends RuntimeException{
+
+
+    public BusinessExceptions(String message){
+        super(message);
+    }
+
+}
